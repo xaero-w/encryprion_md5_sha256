d19b0e37d67fb07e5748bf39586b3775509cb78b
xaero-w <d_shalimov@mail.ru>
2020-05-07 20:40:33 +0300

    add new encryprion SHA2

--- encryprion_md5_sha256.rb
+++ encryprion_md5_sha256.rb
@@ -2,6 +2,7 @@ require 'digest'
 
 md5 = Digest::MD5.new
 sha256 = Digest::SHA256.new
+sha384 = Digest::SHA384.new
 
 puts "Введите слово или фразу для шифрования:"
 user_input = STDIN.gets.chomp
@@ -11,6 +12,7 @@ puts "1. MD5"
 puts "2. SHA1"
 puts "3. SHA2"
 
+
 encryption_method = STDIN.gets.to_i
 
 result =
@@ -19,6 +21,8 @@ result =
     md5.update"#{user_input}"
   when 2
     sha256.update"#{user_input}"
+  when 3
+    sha384.update"#{user_input}"
   end
 
 puts "Вот что получилось: #{result}"
